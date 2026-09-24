bcfdb7aba8c33fcf966206f580dcbf002cf2f8c2
Manyashree <manyashree.hj2005@gmail.com>
2026-09-24 22:55:30 +0530

    removed some code

--- config.py
+++ config.py
@@ -16,12 +16,4 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# import os
 
-# class Config:
-#     SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key")
-#     SQLALCHEMY_DATABASE_URI = os.environ.get(
-#         "DATABASE_URL",
-#         "mysql+pymysql://students:password@localhost/edusupport"
-#     )
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
